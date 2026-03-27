b6b04b7707d8bcb70674a1af046b2adcf52f773f
Santiago <santiagogomez1129@gmail.com>
2026-03-26 22:55:22 -0500

    agregar mensaje por error de peticion con estilos

--- Mini-Ecommerce/src/components/Card.jsx
+++ Mini-Ecommerce/src/components/Card.jsx
@@ -4,15 +4,34 @@ import "../styles/Card.css"
 export const Card = () => {
     const [productos, setProducts] = useState([])
     const [loading, setLoading] =useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         fetch("https://dummyjson.com/products")
-        .then(res => res.json())
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("No se pudieron cargar los productos")
+            }
+            return res.json()
+        })
         .then(data => {
             setProducts(data.products)
             setLoading(false)
         })
+        .catch(err => {
+            setError(err.message)
+            setLoading(false)
+        })
     }, [])
+
+    if (error) {
+        return (
+        <div className="error-box">
+            <h3>Error al cargar productos</h3>
+            <p>{error}</p>
+        </div>
+        )
+    }
 
   return (
     <>
