864b8e83d4f09902501e37402baaf75a2ef295c7
Santiago <santiagogomez1129@gmail.com>
2026-03-27 00:35:04 -0500

    agregar lista de categorias funcional

--- Mini-Ecommerce/src/components/Nav.jsx
+++ Mini-Ecommerce/src/components/Nav.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react"
 import "../styles/Nav.css"
 
-export const Nav = () => {
+export const Nav = ({ onCategoriaChange, categoriaActiva }) => {
+  const [categorias, setCategorias] = useState([])
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products/categories")
+    .then(res => res.json())
+    .then(data => setCategorias(data))
+  }, [])
+
   return (
-    <nav>Nav</nav>
+    <nav>
+      <label htmlFor="categoria">Filtrar por categoría:</label>
+      <select id="categoria" value={categoriaActiva} onChange={(e) => onCategoriaChange(e.target.value)}>
+        <option value="all">All</option>
+        {categorias.map(categoria => (
+          <option key={categoria.slug} value={categoria.slug}>{categoria.name}</option>
+        ))}
+      </select>
+    </nav>
   )
 }
--- Mini-Ecommerce/src/components/Products.jsx
+++ Mini-Ecommerce/src/components/Products.jsx
@@ -12,6 +12,7 @@ export const Products = () => {
   const [productos, setProducts] = useState([])
   const [loading, setLoading] =useState(true)
   const [error, setError] = useState(null)
+  const [categoria, setCategoria] = useState('all')
 
   useEffect(() => {
       fetch("https://dummyjson.com/products")
@@ -31,6 +32,8 @@ export const Products = () => {
       })
   }, [])
 
+  const productosFiltrados = categoria === "all" ? productos : productos.filter(p => p.category === categoria)
+
   if (error) {
       return (
         <section className='products'>
@@ -44,7 +47,8 @@ export const Products = () => {
 
   return (
     <>
-      <Nav />
+      <Nav onCategoriaChange={setCategoria} categoriaActiva={categoria} />
+
       <section className='products'>
         {loading
           ?
@@ -53,7 +57,7 @@ export const Products = () => {
               <h4>Cargando productos...</h4>
           </div>
           :
-          productos.map(producto => (
+          productosFiltrados.map(producto => (
             <Card key={producto.id} producto={producto} />
           ))
         }
